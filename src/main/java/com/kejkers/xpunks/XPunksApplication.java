4cd161a8ac04651b46756e6f39233008aea0831d
Matvey <tod13yung@yandex.ru>
2020-06-04 14:46:35 +0400

    Initial commit x2

---
+++ src/main/java/com/kejkers/xpunks/XPunksApplication.java
@@ -0,0 +1,13 @@
+package com.kejkers.xpunks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XPunksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XPunksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kejkers/xpunks/XPunksApplicationTests.java
@@ -0,0 +1,10 @@
+package com.kejkers.xpunks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPunksApplicationTests {
+
+
+}
